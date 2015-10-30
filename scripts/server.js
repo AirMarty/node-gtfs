e87d0f3b616192251807db959d538b4970dee4b0
Matheo_MSA <matheo.msa@epitech.eu>
2015-10-30 14:51:31 +0100

    Update on shapes import : -shape_id generation upgrade -check if the shapes already exist

--- scripts/server.js
+++ scripts/server.js
@@ -6,18 +6,10 @@ var events = require('events');
 var mongoose = require('mongoose');
 var request = require('request');
 var polyline = require('polyline');
-var fs = require('fs');
+//var fs = require('fs');
 
-//load config.js 
-var config = {};
-var data = fs.readFileSync('./config.json');
-config = JSON.parse(data);
-if (!config.agencies) {
-    handleError(new Error('No network_key specified in config.js\nTry adding \'capital-metro\' to the agencies in config.js to load transit data'));
-    process.exit();
-}
-
-var db = mongoose.connect(config.mongo_url);
+//load config.js
+var db = mongoose.connect('mongodb://localhost:27017/gtfs');
 
 require('../models/Agency');
 require('../models/Calendar');
@@ -95,7 +87,6 @@ var getStopsFoShapes = function(network_key, cb) {
 		    {
 			loc_list.sort(function(a, b){return a.sequence - b.sequence}); //order stops by sequence
 			loc_list = loc_list.map(function(loc){return loc.loc}); // remove sequence from loc_list
-			//console.log(loc_list);
 			callback(e, loc_list);
 		    }
 		    else
@@ -112,11 +103,12 @@ var getStopsFoShapes = function(network_key, cb) {
 	query_url = "http://router.project-osrm.org/trip?"
 	for (var i = 0; i < stop_loc.length; i++){
 	    if (i === 0)
-		query_url = query_url.concat('loc=', JSON.stringify(stop_loc[i][0]), ',', JSON.stringify(stop_loc[i][1]));
+		query_url = query_url.concat('loc=', JSON.stringify(stop_loc[i][0]),
+					     ',', JSON.stringify(stop_loc[i][1]));
 	    else
-		query_url = query_url.concat('&loc=', JSON.stringify(stop_loc[i][0]), ',', JSON.stringify(stop_loc[i][1]));
+		query_url = query_url.concat('&loc=', JSON.stringify(stop_loc[i][0]),
+					     ',', JSON.stringify(stop_loc[i][1]));
 	}
-	// console.log(query_url + '\n');
 	return (query_url);
     }
     
@@ -141,7 +133,6 @@ var getStopsFoShapes = function(network_key, cb) {
 	    } while (b >= 0x20);
 	    var dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
 	    lng += dlng;
-	    //array.push( {lat: lat * precision, lng: lng * precision} );
 	    array.push( [lat * precision, lng * precision] );
 	}
 	return array;
@@ -154,34 +145,77 @@ var getStopsFoShapes = function(network_key, cb) {
 	var i = 0;
 	getTrips(function(e, res){
 	    trip_ids = res.map(function(trip){
-		return trip.trip_id;
+		return {
+		    trip_id : trip.trip_id,
+		    shape_id : trip.shape_id
+		}
 	    })
+	    var tab_compare = [];
 	    trip_ids.forEach(function(exemple, index){
-		getStopLocations(exemple, function(e, res){
+		var pass = false;
+		getStopLocations(exemple.trip_id, function(e, res){
 		    stops_loc.push(res);
 		    var url = buildUrl(res);
 		    request(url, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 			    var test = JSON.parse(body);
-			    test = _decode(test.trips[0].route_geometry, 6);
-			    for (var cmp = 0; cmp < test.length; cmp++){
-				shape = new Shape({
-			    	    shape_id : exemple + i , shape_pt_lat : test[cmp][1],
-				    shape_pt_lon : test[cmp][0],
-				    shape_pt_sequence : cmp,
-				    Comment : " ",  network_key: network_key});
-				shape.save(function(err){
-				    if(err) return handleError(err);
-				})
+			    if (tab_compare.length === 0){
+				tab_compare.push(test.trips[0].route_geometry);
+				pass = true;
+			    }
+			    else{
+				var v = 0;
+				for (var x = 0; x < tab_compare.length; x++){
+				    if (test.trips[0].route_geometry === tab_compare[x]) {
+					v = 1;
+				    }
+				}
+				if (v === 0){
+				    pass = true;
+				}
+			    }
+			    if (pass){
+				test = _decode(test.trips[0].route_geometry, 6);
+				for (var cmp = 0; cmp < test.length; cmp++){
+				    if (!exemple.shape_id){
+					shape = new Shape({
+			    		    shape_id : exemple.trip_id + i,
+					    shape_pt_lat : test[cmp][1],
+					    shape_pt_lon : test[cmp][0],
+					    shape_pt_sequence : cmp,
+					    network_key: network_key});
+					shape.save(function(err){
+					    if(err) return handleError(err);
+					})
+					Trip.update({network_key : network_key, trip_id : exemple.trip_id},
+						    { shape_id : shape.shape_id }, function(err, raw){
+							if(err) return handleError(err);
+							console.log(raw);
+						    })
+				    }
+				    else{
+					shape = new Shape({
+			    		    shape_id : exemple.shape_id,
+					    shape_pt_lat : test[cmp][1],
+					    shape_pt_lon : test[cmp][0],
+					    shape_pt_sequence : cmp,
+					    network_key: network_key});
+					shape.save(function(err){
+					    if(err) return handleError(err);
+					})
+				    }
+				}
+			    }
+			    if (i === trip_ids.length - 1)
+			    {
+				callback(e, stops_loc);
+			    }
+			    else
+			    {
+				i++;
 			    }
 			}
 		    });
-		    if (i === trip_ids.length - 1)
-		    {
-			callback(e, stops_loc);
-		    }
-		    else
-			i++;
 		})
 	    })
 	})
