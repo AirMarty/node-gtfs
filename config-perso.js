b615b0b4d7ba01e34f94fca1eb40b22f37483db7
Matheo_MSA <matheo.msa@epitech.eu>
2015-11-03 17:23:15 +0100

    Now we use "config-perso.js"

--- config-perso.js
+++ config-perso.js
@@ -1,9 +1,11 @@
   module.exports = {
     mongo_url: process.env.MONGO_URL || 'mongodb://localhost:27017/gtfs',
     agencies: [
-//	{agency_key: 'tam', path: './import/montpellier_short'}
-	{territory_key : "Languedoc-Roussillon", network_key: "tam", path: "montpellier_GTFS", parser: function(line, GTFSFile, cb){
-        cb(null, line);
-	}}
+//	{network_key: 'tam_short', path: 'montpellier_short', parser: null}
+	 {network_key: "montpellier_tam", path: "montpellier_GTFS", parser: null}
+	// {territory_key : "Languedoc-Roussillon", network_key: "septa", path: "septa_bus", parser: function(line, GTFSFile, cb){
+        // cb(null, line);
+	// }}
+	// {territory_key : "Languedoc-Roussillon", network_key: "septa", path: "septa_bus", parser: null}
     ]
 };
